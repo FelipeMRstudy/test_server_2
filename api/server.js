f6ec896412e220c556e882c020f8359be47bc32b
FelipeMRstudy <estudiomadrigal2318@gmail.com>
2024-07-09 22:18:09 -0500

    Actualizacion de llamados al servidor

--- api/server.js
+++ api/server.js
@@ -1,20 +1,29 @@
+console.log("Empezo la fiesta, Starting server...");
+
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 console.log("Starting server...");
+console.log("Starting server...");
+console.log("Starting server...");
+console.log("Starting server...");
+console.log("Starting server...");
+console.log("Starting server...");
+console.log("Starting server...");
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/', (req, res) => {
+  console.log("Siguio la fiesta, Starting server...");
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  console.log('GET / Ok HTML');
+});
 
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Hello from the server!, Server ON✅' });
   console.log('GET /api/test request received');
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-  console.log('GET / Ok HTML');
 });
 
 app.listen(port, () => {
